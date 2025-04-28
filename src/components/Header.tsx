ee53f3e84b51055638e3e7e6513a38a6b12d6649
HyunWoo Jeong <jrwedo@naver.com>
2025-04-28 18:55:09 +0900

    Refactor: Header에서 IconButton을 Button으로 대체

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,15 +1,17 @@
 "use client"
 
-import IconButton from '@/components/IconButton';
 import { Menu } from 'lucide-react';
 import { useSidebarStore } from '@/stores/sidebarStore';
+import Button from '@/components/Button';
 
 export default function Header() {
   const openSidebar = useSidebarStore((state) => state.open);
 
   return (
     <header>
-      <IconButton icon={Menu} onClick={openSidebar} />
+      <Button variant='ghost' onClick={openSidebar}>
+        <Menu/>
+      </Button>
     </header>
   );
 }

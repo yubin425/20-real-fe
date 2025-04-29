c5e9f31b56fd7cb1a6ee866fe53d68609f089560
HyunWoo Jeong <jrwedo@naver.com>
2025-04-29 15:09:45 +0900

    Feat: 헤더 알림 버튼 추가

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { Menu } from 'lucide-react';
+import { Menu, Bell } from 'lucide-react';
 import { useSidebarStore } from '@/stores/sidebarStore';
 import Button from '@/components/Button';
 import { HEADER_HEIGHT } from '@/constatns/ui';
@@ -9,9 +9,13 @@ export default function Header() {
   const openSidebar = useSidebarStore((state) => state.open);
 
   return (
-    <header style={{height: `${HEADER_HEIGHT}px`}}>
+    <header className="flex flex-row justify-between items-center" style={{height: `${HEADER_HEIGHT}px`}}>
       <Button variant="ghost" size="lg_icon" onClick={openSidebar}>
         <Menu />
+      </Button>
+
+      <Button variant="ghost" size="lg_icon">
+        <Bell />
       </Button>
     </header>
   );

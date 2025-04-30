07230a3509cbbbe625855979dee5ba9c7ce49e6f
HyunWoo Jeong <jrwedo@naver.com>
2025-05-01 08:58:27 +0900

    Feat: 헤더에 뒤로가기 버튼 추가 및 사이드바 우측으로 이동

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -3,6 +3,7 @@
 import { SidebarStore, useSidebarStore } from '@/stores/sidebarStore';
 import Link from 'next/link';
 import { useEffect } from 'react';
+import { APP_WIDTH } from '@/constatns/ui';
 
 export default function Sidebar() {
   const isOpen = useSidebarStore((state: SidebarStore) => state.isOpen);
@@ -28,29 +29,32 @@ export default function Sidebar() {
     <>
       {isOpen && (
         <div
-          className="fixed inset-0 bg-black/30 z-40"
+          className={`fixed top-0 max-w-app w-full h-full bg-black/30 text-white z-40 transition-transform duration-200 ${
+            isOpen ? 'translate-x-0' : '-translate-x-full'
+          }`}
           onClick={close}
         />
       )}
 
-        <aside
-          className={`fixed top-0 left-0 w-52 h-full bg-gray-700 text-white z-50 transition-transform duration-300 ${
-            isOpen ? 'translate-x-0' : '-translate-x-full'
-          }`}
-        >
-          <nav className="p-4 flex flex-col ">
-            {menuItems.map(({ label, href }) => (
-              <Link
-                key={href}
-                href={href}
-                onClick={close}
-                className="text-white hover:bg-gray-500 text-left py-2 px-4 rounded-md"
-              >
-                {label}
-              </Link>
-            ))}
-          </nav>
-        </aside>
+      <aside
+        className={`absolute top-0 w-[200px] h-full bg-gray-700 text-white z-50
+    transition-all duration-200
+    ${isOpen ? 'opacity-100 visible pointer-events-auto' : 'opacity-0 invisible pointer-events-none'}`}
+        style={{left: `calc(${APP_WIDTH}px - 200px)`}}
+      >
+        <nav className="p-4 flex flex-col ">
+          {menuItems.map(({ label, href }) => (
+            <Link
+              key={href}
+              href={href}
+              onClick={close}
+              className="text-white hover:bg-gray-500 text-left py-2 px-4 rounded-md"
+            >
+              {label}
+            </Link>
+          ))}
+        </nav>
+      </aside>
     </>
   );
 }

bfd75ddb0473a570750d68b43fd5104e3a57a1dd
HyunWoo Jeong <jrwedo@naver.com>
2025-05-05 20:19:42 +0900

    Refactor: Skeleton의 재사용성을 위해 SkeletonBox로 리팩토링

--- src/components/chatbot/HeadlineBannerSkeleton.tsx
+++ src/components/chatbot/HeadlineBannerSkeleton.tsx
@@ -1,15 +1,12 @@
+import SkeletonBox from '@/components/common/SkeletonBox';
+
 export default function HeadlineBannerSkeleton() {
   return (
     <div className="bg-white mx-4 mt-4 rounded-xl shadow-sm overflow-hidden animate-pulse">
       <div className="h-12 px-4 flex items-center space-x-3">
-        {/* 아이콘 */}
-        <div className="w-6 h-6 rounded-full bg-gray-300 flex-shrink-0" />
-
-        {/* 텍스트 */}
-        <div className="flex-1 h-4 bg-gray-200 rounded" />
-
-        {/* 인덱스 */}
-        <div className="w-10 h-3 bg-gray-200 rounded" />
+        <SkeletonBox className="w-6 h-6 rounded-full flex-shrink-0" />
+        <SkeletonBox className="flex-1 h-4" />
+        <SkeletonBox className="w-10 h-3" />
       </div>
     </div>
   );

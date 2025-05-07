f831bdfe683cb0ca985754042a71d9b7dd843395
HyunWoo Jeong <jrwedo@naver.com>
2025-05-07 14:15:24 +0900

    Feat: 공지에서 첨부파일 클릭 시 파일 다운로드 하도록 구현

--- src/components/post/PostFileItem.tsx
+++ src/components/post/PostFileItem.tsx
@@ -10,7 +10,7 @@ export default function PostFileItem({ file }: PostFileItemProps) {
     <div className="mb-4 bg-gray-50 rounded-lg p-3">
       <div className="flex items-center text-gray-600 text-sm">
         <Paperclip size={16} className="mr-2 text-gray-500" />
-        <span>{file.fileName}</span>
+        <span>{file.fileName}.{file.fileType}</span>
       </div>
     </div>
   )

8116d1ddebaecbf4ca5afd0798ac225f2425e01f
HyunWoo Jeong <jrwedo@naver.com>
2025-05-07 19:12:48 +0900

    Lint: fetcher 에러 발생 시 log출력을 error로 변경

--- src/api/fetcher.ts
+++ src/api/fetcher.ts
@@ -1,32 +1,27 @@
-import { useUserPersistStore } from '@/stores/userPersistStore';
-import { useToastStore } from '@/stores/toastStore';
 import { errors } from '@/constatns/errors';
+import { useToastStore } from '@/stores/toastStore';
+import { useUserPersistStore } from '@/stores/userPersistStore';
 
-export async function fetcher<T>(
-  url: string,
-  options?: RequestInit,
-): Promise<T> {
+export async function fetcher<T>(url: string, options?: RequestInit): Promise<T> {
   const timeout = url.includes('/v1/chatbots') ? 60000 : 5000;
-  const res = await fetch(
-    process.env.NEXT_PUBLIC_API_URL + url,
-    {
-      ...options,
-      headers: {
-        'Content-Type': 'application/json',
-        ...(options?.headers ?? {}),
-      },
-      credentials: 'include',
-      signal: AbortSignal.timeout(timeout)
-    });
+  const res = await fetch(process.env.NEXT_PUBLIC_API_URL + url, {
+    ...options,
+    headers: {
+      'Content-Type': 'application/json',
+      ...(options?.headers ?? {}),
+    },
+    credentials: 'include',
+    signal: AbortSignal.timeout(timeout),
+  });
 
   if (res.status === 401) {
     useUserPersistStore.getState().cleanUser();
-    useToastStore.getState().showToast(errors.UNAUTHORIZED, 'error')
+    useToastStore.getState().showToast(errors.UNAUTHORIZED, 'error');
   } else if (res.status === 403) {
     useUserPersistStore.getState().setIsApproved(false);
-    useToastStore.getState().showToast(errors.FORBIDDEN, 'error')
+    useToastStore.getState().showToast(errors.FORBIDDEN, 'error');
   } else if (!res.ok) {
-    let message = errors.DEFAULT
+    let message = errors.DEFAULT;
 
     try {
       const errorBody = await res.json();
@@ -34,10 +29,10 @@ export async function fetcher<T>(
         message = errorBody.message;
       }
     } catch (e) {
-      console.log(e);
+      console.error(e);
     }
 
-    useToastStore.getState().showToast(message, 'error')
+    useToastStore.getState().showToast(message, 'error');
   }
 
   return res.json();

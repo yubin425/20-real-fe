7d026b026470d10b4827cb8be826062fdea2a95c
HyunWoo Jeong <jrwedo@naver.com>
2025-05-08 19:29:00 +0900

    Feat: 공지 편집/삭제 기능 구현

--- src/app/(withHeader)/admin/notices/edit/[id]/page.tsx
+++ src/app/(withHeader)/admin/notices/edit/[id]/page.tsx
@@ -3,5 +3,10 @@
 import AdminNoticeForm from '@/app/(withHeader)/admin/notices/_components/AdminNoticeForm';
 
 export default function AdminNoticeNewPage() {
-  return <AdminNoticeForm type="new" />;
+  return (
+    <div>
+      <AdminNoticeForm type="edit" />
+      <div></div>
+    </div>
+  );
 }

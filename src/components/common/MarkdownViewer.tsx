d11b882da8d9a9c2a2d8e97f90b5c9f100d49596
HyunWoo Jeong <jrwedo@naver.com>
2025-05-08 17:04:31 +0900

    Feat: remarkGfm플러그인을 사용한 표, 취소선 구현

--- src/components/common/MarkdownViewer.tsx
+++ src/components/common/MarkdownViewer.tsx
@@ -1,5 +1,7 @@
+import remarkGfm from 'remark-gfm';
+
 import Markdown from 'react-markdown';
-import {Prism as SyntaxHighlighter} from 'react-syntax-highlighter'
+import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
 
 interface MarkdownViewerProps {
   text: string;
@@ -9,6 +11,7 @@ export default function MarkdownViewer({ text }: MarkdownViewerProps) {
   return (
     <div className="text-black text-sm/6">
       <Markdown
+        remarkPlugins={[remarkGfm]}
         components={{
           h1: (props) => <h1 className="text-2xl font-bold mt-6 pb-2 mb-2 border-b-1 border-gray-300" {...props} />,
           h2: (props) => <h2 className="text-xl font-bold mt-5 pb-2 mb-2 border-b-1 border-gray-300" {...props} />,
@@ -22,25 +25,25 @@ export default function MarkdownViewer({ text }: MarkdownViewerProps) {
           blockquote: (props) => (
             <blockquote className="border-l-4 border-gray-300 pl-4 italic text-gray-600 my-4" {...props} />
           ),
+          table: (props) => <table className="table-auto border border-gray-600 border-collapse w-full" {...props} />,
+          th: (props) => <th className="border border-gray-600 px-2 py-1 bg-gray-100 text-left" {...props} />,
+          td: (props) => <td className="border border-gray-600 px-2 py-1" {...props} />,
+          aside: (props) => <aside {...props} />,
+          tr: (props) => <tr className="even:bg-gray-50" {...props} />,
           code: ({ children, className }) => {
             const match = /language-(\w+)/.exec(className || '');
             const codeString = String(children).replace(/\n$/, '');
 
             if (match) {
               return (
-                <SyntaxHighlighter
-                  language={match[1]}
-                  PreTag="div"
-                >
+                <SyntaxHighlighter language={match[1]} PreTag="div">
                   {codeString}
                 </SyntaxHighlighter>
               );
             }
 
             return (
-              <code className="bg-gray-100 px-1 py-0.5 rounded text-xs font-mono text-primary-700">
-                {codeString}
-              </code>
+              <code className="bg-gray-100 px-1 py-0.5 rounded text-xs font-mono text-primary-700">{codeString}</code>
             );
           },
         }}
@@ -49,5 +52,4 @@ export default function MarkdownViewer({ text }: MarkdownViewerProps) {
       </Markdown>
     </div>
   );
-
 }

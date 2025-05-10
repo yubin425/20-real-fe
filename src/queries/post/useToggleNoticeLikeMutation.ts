0f75c8bb24373f141b767cfc687f9956e0b81df9
HyunWoo Jeong <jrwedo@naver.com>
2025-05-10 15:25:53 +0900

    Refactor: 게시글 상세 화면을 SSR로 전환하기 위해 내부 상태를 가진 컴포넌트를 별도의 공통 컴포넌트로 분리

--- src/queries/post/useToggleNoticeLikeMutation.ts
+++ src/queries/post/useToggleNoticeLikeMutation.ts
@@ -1,10 +1,6 @@
 import { useMutation } from '@tanstack/react-query';
 
 import { toggleNoticeLike } from '@/api/post';
-import { queryKeys } from '@/constatns/keys';
-import { queryClient } from '@/queries/base/queryClient';
-import { BaseResponse } from '@/types/common/base';
-import { NoticeDetail } from '@/types/post/noticeDetail';
 
 interface ToggleNoticeLikeRequest {
   noticeId: string;
@@ -13,22 +9,5 @@ interface ToggleNoticeLikeRequest {
 export function useToggleNoticeLikeMutation() {
   return useMutation({
     mutationFn: ({ noticeId }: ToggleNoticeLikeRequest) => toggleNoticeLike(noticeId),
-    // 토글 성공 후 데이터 갱신
-    onSuccess: (_data, variables) => {
-      queryClient.setQueryData(
-        [queryKeys.notice, variables.noticeId],
-        (prev: BaseResponse<NoticeDetail> | undefined) => {
-          if (!prev) return prev;
-          return {
-            ...prev,
-            data: {
-              ...prev.data,
-              userLike: !prev.data.userLike,
-              likeCount: prev.data.userLike ? prev.data.likeCount - 1 : prev.data.likeCount + 1,
-            },
-          };
-        },
-      );
-    },
   });
 }
---
+++ src/types/post/postType.ts
@@ -0,0 +1,4 @@
+export enum PostTypes {
+  Notice,
+  News,
+}

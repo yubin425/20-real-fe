24eec763f35807f30545459fbc4f52f4455e0546
HyunWoo Jeong <jrwedo@naver.com>
2025-05-22 15:25:11 +0900

    Refactor: 게시글 상세 CSR로 전환

--- src/components/post/pages/NewsDetailPage/NewsDetailPage.tsx
+++ src/components/post/pages/NewsDetailPage/NewsDetailPage.tsx
@@ -4,7 +4,9 @@ import { useParams } from 'next/navigation';
 
 import { ImageViewer } from '@/components/common/molecules/ImageViewer';
 import { MarkdownViewer } from '@/components/common/molecules/MarkdownViewer';
+import { ErrorPage } from '@/components/common/pages/ErrorPage';
 import { NotFoundPage } from '@/components/common/pages/NotFoundPage';
+import { RedirectWithLoginModalPage } from '@/components/common/pages/RedirectWithLoginModalPage';
 import { PostHeader } from '@/components/post/molecules/PostHeader';
 import { PostSummary } from '@/components/post/molecules/PostSummary';
 import { PostCommentSection } from '@/components/post/organisms/PostCommentSection';
@@ -15,11 +17,17 @@ import { PostTypes } from '@/types/post/postType';
 export function NewsDetailPage() {
   const params = useParams();
   const id: string = params?.id as string;
-  const { data: news, isLoading } = useNewsDetailQuery(id);
+  const { data: news, isLoading, isError, error } = useNewsDetailQuery(id);
 
   if (isLoading) return null;
-  // if (response?.code === 401 || response?.code === 403) return <RedirectWithLoginModalPage/>
-  if (!news) return <NotFoundPage />;
+  if (isError) {
+    switch (error?.code) {
+      case 'UNAUTHORIZED': return <RedirectWithLoginModalPage/>
+      case 'NOT_FOUND': return <NotFoundPage/>
+      default: return <ErrorPage/>
+    }
+  }
+  if (!news) return <ErrorPage />;
 
   return (
     <div className="flex justify-center items-center w-full">
